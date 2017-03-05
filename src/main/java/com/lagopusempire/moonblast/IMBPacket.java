dca8260cee7863991be25fc9b61fc3efe887989d
Matt <MrZoraman@Gmail.com>
2017-03-04 19:13:21 -0700

    IMBPacket fields added.

--- src/main/java/com/lagopusempire/moonblast/IMBPacket.java
+++ src/main/java/com/lagopusempire/moonblast/IMBPacket.java
@@ -1,6 +1,9 @@
 package com.lagopusempire.moonblast;
 
 import com.lagopusempire.moonblast.params.IMBParam;
+import com.lagopusempire.moonblast.params.IntParam;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IMBPacket {
     static final byte PACKET_START = '(';
@@ -8,10 +11,13 @@ public class IMBPacket {
     
     static final int VERSION = 1;
     
+    private final List<IMBParam> params = new ArrayList<>();
     
+    public IMBPacket() {
+    }
     
     public IMBParam[] getParams() {
-        return null;
+        return params.toArray(new IMBParam[0]);
     }
     
     public int getParamsLength() {
@@ -19,6 +25,28 @@ public class IMBPacket {
     }
     
     byte[] getData() {
+        
         return null;
     }
+    
+    public void addIMBParam(IMBParam param) {
+        params.add(param);
+    }
+    
+    private int getPacketLengthInBytes() {
+        return 0;
+    }
+    
+//    public IMBPacket addByte(byte value);
+//    public IMBPacket addShort(short value);
+    public IMBPacket addInt(int value) {
+        addIMBParam(new IntParam(value));
+        return this;
+    }
+//    public IMBPacket addLong(long value);
+//    public IMBPacket addFloat(float value);
+//    public IMBPacket addDouble(double value);
+//    public IMBPacket addBoolean(boolean value);
+//    public IMBPacket addChar(char value);
+//    public IMBPacket addBinary(byte[] value);
 }
