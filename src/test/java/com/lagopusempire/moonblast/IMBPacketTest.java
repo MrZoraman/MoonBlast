4fa0fb153d4f66f61976eded37db8cd4211db5ab
Matt <MrZoraman@Gmail.com>
2017-03-04 19:27:06 -0700

    Added test

---
+++ src/test/java/com/lagopusempire/moonblast/IMBPacketTest.java
@@ -0,0 +1,26 @@
+package com.lagopusempire.moonblast;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class IMBPacketTest {
+    
+    public IMBPacketTest() {
+    }
+    
+    @Test
+    public void testPacketLength() {
+        IMBPacket packet = new IMBPacket();
+        assertEquals(1 + 8 + 4 + 4 + 1, packet.getPacketLengthInBytes());
+    }
+    
+    @Test
+    public void testPacketLengthWithParams() {
+        IMBPacket packet = new IMBPacket();
+        packet.addInt(57);
+        packet.addInt(632);
+        packet.addInt(-4);
+        assertEquals(1 + 8 + 4 + 4 + (1 * 3) + (4 + 4 + 4) + 1, packet.getPacketLengthInBytes());
+    }
+    
+}
