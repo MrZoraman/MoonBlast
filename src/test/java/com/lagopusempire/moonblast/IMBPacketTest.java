4735ad9d8591cc968e9c3c1fdfdfcfe2261e0c5c
Matt <MrZoraman@Gmail.com>
2017-03-04 20:22:13 -0700

    deserialization works!

--- src/test/java/com/lagopusempire/moonblast/IMBPacketTest.java
+++ src/test/java/com/lagopusempire/moonblast/IMBPacketTest.java
@@ -1,5 +1,7 @@
 package com.lagopusempire.moonblast;
 
+import com.lagopusempire.moonblast.params.IMBParam;
+import com.lagopusempire.moonblast.params.IntParam;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -24,15 +26,20 @@ public class IMBPacketTest {
     }
     
     @Test
-    public void testPacketWithInts() {
-        IMBPacket packet = new IMBPacket();
-        packet.addInt(1);
-        //assertEquals(1 + 4 + 4 + 4 + (1 * 1) + (4) + 1, packet.getPacketLengthInBytes());
-        
-        byte[] packetData = packet.getData();
-        byte[] expectedData = new byte[] {
-            '('
+    public void testPacketIntDeserialization() {
+        byte[] data = new byte[] {
+            0, 0, 0, 1, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, -117, 39, 41 
         };
+        
+        IMBPacket packet = new IMBPacket(data);
+        IMBParam[] params = packet.getParams();
+        assertEquals(2, params.length);
+        
+        IntParam p0 = (IntParam)params[0];
+        IntParam p1 = (IntParam)params[1];
+        
+        assertEquals(0, p0.getInt());
+        assertEquals(35623, p1.getInt());
     }
     
 }
