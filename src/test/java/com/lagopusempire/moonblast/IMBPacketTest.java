4770cbf0d7450c2e2eb316fa91a0db4c886c2151
Matt <MrZoraman@Gmail.com>
2017-03-16 21:52:40 -0700

    The Big Test passes!

--- src/test/java/com/lagopusempire/moonblast/IMBPacketTest.java
+++ src/test/java/com/lagopusempire/moonblast/IMBPacketTest.java
@@ -1,9 +1,6 @@
 package com.lagopusempire.moonblast;
 
-import com.lagopusempire.moonblast.params.BinaryParam;
-import com.lagopusempire.moonblast.params.IMBParam;
-import com.lagopusempire.moonblast.params.IntParam;
-import com.lagopusempire.moonblast.params.ParamType;
+import com.lagopusempire.moonblast.params.*;
 import java.util.Arrays;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -112,5 +109,48 @@ public class IMBPacketTest {
         assertArrayEquals(new byte[]{1, 2, 3}, ((BinaryParam)params[1]).getBinary());
         assertArrayEquals(new byte[]{4, 5, 6}, ((BinaryParam)params[3]).getBinary());
         assertArrayEquals(new byte[]{7, 8, 9}, ((BinaryParam)params[4]).getBinary());
+    }
+    
+    @Test
+    public void theBigTest() {
+        IMBPacket packet = new IMBPacket();
+        packet.addByte((byte)-53)
+                .addShort((short)25560)
+                .addInt(245667000)
+                .addLong(2020506506L)
+                .addFloat(432.53f)
+                .addDouble(222.45560600)
+                .addBoolean(false)
+                .addChar('j')
+                .addBinary(new byte[]{1, -4, 2, 33})
+                .addString("testing 123");
+        byte[] b = packet.getData();
+        byte[] b2 = Arrays.copyOfRange(b, ParamType.INT.getSizeInBytes() + 1, b.length);
+        
+        packet = new IMBPacket(b2);
+        IMBParam[] params = packet.getParams();
+        assertEquals(10, params.length);
+        
+        assertEquals(ParamType.BYTE, params[0].getType());
+        assertEquals(ParamType.SHORT, params[1].getType());
+        assertEquals(ParamType.INT, params[2].getType());
+        assertEquals(ParamType.LONG, params[3].getType());
+        assertEquals(ParamType.FLOAT, params[4].getType());
+        assertEquals(ParamType.DOUBLE, params[5].getType());
+        assertEquals(ParamType.BOOLEAN, params[6].getType());
+        assertEquals(ParamType.CHAR, params[7].getType());
+        assertEquals(ParamType.BINARY, params[8].getType());
+        assertEquals(ParamType.STRING, params[9].getType());
+        
+        assertEquals((byte)-53, ((ByteParam)params[0]).getByte());
+        assertEquals((short)25560, ((ShortParam)params[1]).getShort());
+        assertEquals(245667000, ((IntParam)params[2]).getInt());
+        assertEquals(2020506506L, ((LongParam)params[3]).getLong());
+        assertEquals(432.53f, ((FloatParam)params[4]).getFloat(), 0.01);
+        assertEquals(222.45560600, ((DoubleParam)params[5]).getDouble(), 0.01);
+        assertEquals(false, ((BooleanParam)params[6]).getBoolean());
+        assertEquals('j', ((CharParam)params[7]).getChar());
+        assertArrayEquals(new byte[]{1, -4, 2, 33}, ((BinaryParam)params[8]).getBinary());
+        assertEquals("testing 123", ((StringParam)params[9]).getString());
     }
 }
