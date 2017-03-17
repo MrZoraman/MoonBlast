bacb763ad78745bfa943c5a815b2a656e5d9ee8b
Matt <MrZoraman@Gmail.com>
2017-03-16 23:52:15 -0700

    all nice and pretty~

--- src/main/java/com/lagopusempire/moonblast/params/ParamType.java
+++ src/main/java/com/lagopusempire/moonblast/params/ParamType.java
@@ -3,16 +3,16 @@ package com.lagopusempire.moonblast.params;
 import com.lagopusempire.moonblast.IDeserializer;
 
 public enum ParamType {
-    BYTE    (0, Byte.SIZE / 8, (params, buffer) -> params.add(new ByteParam(buffer))),
-    SHORT   (1, Short.SIZE / 8, (params, buffer) -> params.add(new ShortParam(buffer))),
-    INT     (2, Integer.SIZE / 8, (params, buffer) -> params.add(new IntParam(buffer))),
-    LONG    (3, Long.SIZE / 8, (params, buffer) -> params.add(new LongParam(buffer))),
-    FLOAT   (4, Float.SIZE / 8, (params, buffer) -> params.add(new FloatParam(buffer))),
-    DOUBLE  (5, Double.SIZE / 8, (params, buffer) -> params.add(new DoubleParam(buffer))),
-    BOOLEAN (6, 1, (params, buffer) -> params.add(new BooleanParam(buffer))),
+    BYTE    (0, Byte.SIZE / 8,      (params, buffer) -> params.add(new ByteParam(buffer))),
+    SHORT   (1, Short.SIZE / 8,     (params, buffer) -> params.add(new ShortParam(buffer))),
+    INT     (2, Integer.SIZE / 8,   (params, buffer) -> params.add(new IntParam(buffer))),
+    LONG    (3, Long.SIZE / 8,      (params, buffer) -> params.add(new LongParam(buffer))),
+    FLOAT   (4, Float.SIZE / 8,     (params, buffer) -> params.add(new FloatParam(buffer))),
+    DOUBLE  (5, Double.SIZE / 8,    (params, buffer) -> params.add(new DoubleParam(buffer))),
+    BOOLEAN (6, 1,                  (params, buffer) -> params.add(new BooleanParam(buffer))),
     CHAR    (7, Character.SIZE / 8, (params, buffer) -> params.add(new CharParam(buffer))),
-    BINARY  (8, -1, (params, buffer) -> params.add(new BinaryParam(buffer))),
-    STRING  (9, -1, (params, buffer) -> params.add(new StringParam(buffer)));
+    BINARY  (8, -1,                 (params, buffer) -> params.add(new BinaryParam(buffer))),
+    STRING  (9, -1,                 (params, buffer) -> params.add(new StringParam(buffer)));
     
     private static final ParamType[] TYPES = {
         BYTE,
